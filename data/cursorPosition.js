7c3237042687bbd5dd3ac7327833ed5ae0595918
Hainee-tyan <cancertyan@mail.ru>
2015-05-21 13:01:53 +1000

    Moved function to find word on specified coordinates to single file, now it interacts with main content script, that track mouse position and keyboard events

--- data/cursorPosition.js
+++ data/cursorPosition.js
@@ -12,93 +12,13 @@
 		y = event.clientY;
 	});
 
-	//when we get a message from main add-on script, we should
-	//send coordinates back to it
+	//when user press key, we invoke function to find word 
+	//on position of cursor and sent position itself and word
+	//to add-on main script
 	window.addEventListener("keydown", function(event) {
-		if (event.which === keyToPress && event.location === keyLocation)
-			self.port.emit("cursorPosition", x, y);
+		if (event.which === keyToPress && event.location === keyLocation) {
+			var word = getWordOnCoords(window.document.body, x, y);
+			self.port.emit("cursorPosition", x, y, word);
+		};
 	});
 })();
-
-
-//----------------------------
-function() {
-window.addEventListener("click", function(event) {
-
-    var x = event.clientX;
-    var y = event.clientY;
-    
-    var word = getWordUnderCursor(window.document.body, x, y);
-    
-    document.getElementById("rectlist").innerHTML = word;
-});
-
-
-//return word or null if there were no word on such coords
-function getWordUnderCursor(node, x, y) {    
-    var range = document.createRange();
-    range.selectNode(node);
-    
-    //check if our coords are inside element
-    if (coordsInRange(range, x, y) != 0) return null;
-    
-    //Work with text itself
-    if (node.nodeType == document.TEXT_NODE) {
-        //split text into words
-        var strings = node.nodeValue.split(/[^a-zA-Z0-9]/);
-        
-        //start and end position of range
-        var startOffset = 0;
-        var endOffset = 0;
-
-        //check range of every word in node
-        for (var i = 0; i < strings.length; i++) {
-            endOffset = startOffset + strings[i].length;
-            
-            //set new range, which is single word
-            range.setStart(node, startOffset);
-            range.setEnd(node, endOffset);
-        
-            //if our word is in range, we siply return it
-            if (coordsInRange(range, x, y) == 0)
-            {
-                var word = range.toString();
-                //detack range for oprimization
-                range.detach();
-                return word;
-            };
-            
-            //if we have already passed our coords,
-            //but there was no word, return null
-            if (coordsInRange(range, x, y) > 0) return null;
-
-            startOffset = endOffset + 1;      
-        };
-    }
-    else {
-        //check every child of our node
-        var nodeList = node.childNodes;
-        for (var i = 0; i < nodeList.length; i++)
-        {
-            //some recursion
-            var word = getWordUnderCursor(nodeList[i], x, y);
-            if (word != null) return word;
-        };
-    }; 
-    return null;
-};
-
-//if coords are in range - return 0,
-//if coords are before range - return -1,
-//if coords are after range - return 1
-function coordsInRange(range, x, y) {
-    var rect = range.getBoundingClientRect();
-    if (rect.top <= y && rect.bottom >= y &&
-        rect.left <= x && rect.right >= x)
-        return 0;
-    if (rect.top > y || rect.left  >x) 
-        return 1;
-    return -1;
-};
-
-};
---
+++ data/getWordOnCoords.js
@@ -0,0 +1,71 @@
+﻿//return word or null if there were no word on such coords
+function getWordOnCoords(node, x, y) {    
+    var range = document.createRange();
+    range.selectNode(node);
+    
+    //check if our coords are inside element
+    if (coordsInRange(range, x, y) != 0) return null;
+    
+    //Work with text itself
+    if (node.nodeType == document.TEXT_NODE) {
+        //split text into words
+        var strings = node.nodeValue.split(/[^a-zA-Z0-9]/);
+        
+        //start and end position of range
+        var startOffset = 0;
+        var endOffset = 0;
+
+        //check range of every word in node
+        for (var i = 0; i < strings.length; i++) {
+            endOffset = startOffset + strings[i].length;
+            
+            //set new range, which is single word
+            range.setStart(node, startOffset);
+            range.setEnd(node, endOffset);
+        
+            //if our word is in range, we simply return it
+            if (coordsInRange(range, x, y) == 0) {
+                var word = range.toString();
+                //detach range for optimization
+                range.detach();
+                return word;
+            };
+            
+            //if we have already passed our coords,
+            //but there was no word, return null
+            if (coordsInRange(range, x, y) > 0) {
+				range.detach();
+				return null;
+			};
+
+            startOffset = endOffset + 1;      
+        };
+    }
+    else {
+        //check every child of our node
+        var nodeList = node.childNodes;
+        for (var i = 0; i < nodeList.length; i++) {
+            //some recursion
+            var word = getWordOnCoords(nodeList[i], x, y);
+            if (word != null) {
+				range.detach();
+				return word;
+			};
+        };
+    };
+	range.detach();
+    return null;
+};
+
+//if coords are in range - return 0,
+//if coords are before range - return -1,
+//if coords are after range - return 1
+function coordsInRange(range, x, y) {
+    var rect = range.getBoundingClientRect();
+    if (rect.top <= y && rect.bottom >= y &&
+        rect.left <= x && rect.right >= x)
+        return 0;
+    if (rect.top > y) 
+        return 1;
+    return -1;
+};
